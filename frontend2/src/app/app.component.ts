44d13598c82572ffd20a2af7a0f620eea0ce9523
Łukasz Tenerowicz <konkit@gmail.com>
2024-11-24 18:28:04 +0100

    Fix date selector

--- frontend2/src/app/app.component.ts
+++ frontend2/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, model, OnInit, signal} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
 import {MatMenuModule} from '@angular/material/menu'
@@ -8,7 +8,7 @@ import {MatInputModule} from '@angular/material/input'
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {AddDay, DayOfWeek, Today} from "../lib/wall_date";
 import {TaskService} from "../service/task.service";
-import {map, switchMap, take, tap} from "rxjs";
+import {switchMap, tap} from "rxjs";
 import {CommonModule} from '@angular/common';
 import {Task} from "../lib/task";
 import {TaskmenuComponent} from './components/taskmenu/taskmenu.component';
@@ -22,7 +22,7 @@ import {FormsModule} from '@angular/forms';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  title = 'frontend2';
+  title = 'Programmer Journal';
   protected readonly DayOfWeek = DayOfWeek;
 
   todayDate = signal<string>(Today());
@@ -38,10 +38,12 @@ export class AppComponent implements OnInit {
 
   changeDateForward() {
     this.todayDate.update((oldVal) => AddDay(oldVal, 1))
+    this.refreshTasks().subscribe()
   }
 
   changeDateBackward() {
     this.todayDate.update((oldVal) => AddDay(oldVal, -1))
+    this.refreshTasks().subscribe()
   }
 
   submitTitleEditWithValue(task: Task, e: Event) {
