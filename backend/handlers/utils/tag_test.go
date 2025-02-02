0f14e0136a94874e7f085121e6adcf9834673f57
Łukasz Tenerowicz <konkit@gmail.com>
2025-02-02 14:47:05 +0100

    Improve tests for SaveTags

--- backend/handlers/utils/tag_test.go
+++ backend/handlers/utils/tag_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"gorm.io/gorm"
@@ -12,182 +13,69 @@ import (
 )
 
 func TestSaveTags(t *testing.T) {
-	dbTestPath, db := initDB()
-	defer os.Remove(dbTestPath)
-
-	initEntry := entry.Entry{
-		Title: "Something something #tagone",
-	}
-	err := initializeEntry(db, initEntry)
-	if err != nil {
-		t.Fatalf("error saving initial entry: %v", err)
-	}
-
-	initTags := []tag.Tag{}
-	err = initializeTags(db, initTags, err)
-	if err != nil {
-		t.Fatalf("error saving initial tags: %v", err)
-	}
-
-	initEntryTags := []tag.EntryTag{}
-	err = initializeEntryTags(db, initEntryTags, err)
-	if err != nil {
-		t.Fatalf("error saving initial entryTags: %v", err)
-	}
-
-	err = SaveTags(db, initEntry)
-	if err != nil {
-		t.Fatalf("Error saving tags: %v", err)
-	}
-
-	tags := []tag.Tag{}
-	db.Model(tag.Tag{}).Find(&tags)
-	wantTags := []tag.Tag{
+	testCases := []struct {
+		name           string
+		initEntryTitle string
+		initTags       []string
+		initTagEntries []string
+		wantTags       []string
+		wantTagEntries []string
+	}{
 		{
-			Name: "tagone",
-		},
-	}
-	if diff := cmp.Diff(wantTags, tags, cmpopts.IgnoreFields(tag.Tag{}, "ID")); diff != "" {
-		t.Errorf("Tags mismatch (-want +got):\n%s", diff)
-	}
-
-	entryTags := []tag.EntryTag{}
-	db.Model(tag.EntryTag{}).Find(&entryTags)
-	wantEntryTag := []tag.EntryTag{
-		{
-			EntryID: initEntry.ID,
-			TagID:   tags[0].ID,
-		},
-	}
-	if diff := cmp.Diff(wantEntryTag, entryTags, cmpopts.IgnoreFields(tag.EntryTag{}, "ID")); diff != "" {
-		t.Errorf("EntryTags mismatch (-want +got):\n%s", diff)
-	}
-}
-
-func TestSaveTagsTagExists(t *testing.T) {
-	dbTestPath, db := initDB()
-	defer os.Remove(dbTestPath)
-
-	initEntry := entry.Entry{
-		Title: "Something something #tagone",
-	}
-	err := initializeEntry(db, initEntry)
-	if err != nil {
-		t.Fatalf("error saving initial entry: %v", err)
-	}
-
-	initTags := []tag.Tag{
-		{
-			Name: "tagone",
-		},
-	}
-	err = initializeTags(db, initTags, err)
-	if err != nil {
-		t.Fatalf("error saving initial tags: %v", err)
-	}
-
-	initEntryTags := []tag.EntryTag{
-		{
-			EntryID: initEntry.ID,
-			TagID:   initTags[0].ID,
-		},
-	}
-	err = initializeEntryTags(db, initEntryTags, err)
-	if err != nil {
-		t.Fatalf("error saving initial entryTags: %v", err)
-	}
-
-	err = SaveTags(db, initEntry)
-	if err != nil {
-		t.Fatalf("Error saving tags: %v", err)
-	}
-
-	tags := []tag.Tag{}
-	db.Model(tag.Tag{}).Find(&tags)
-	wantTags := []tag.Tag{
-		{
-			Name: "tagone",
-		},
-	}
-	if diff := cmp.Diff(wantTags, tags, cmpopts.IgnoreFields(tag.Tag{}, "ID")); diff != "" {
-		t.Errorf("Tags mismatch (-want +got):\n%s", diff)
-	}
-
-	entryTags := []tag.EntryTag{}
-	db.Model(tag.EntryTag{}).Find(&entryTags)
-	wantEntryTag := []tag.EntryTag{
-		{
-			EntryID: initEntry.ID,
-			TagID:   tags[0].ID,
-		},
-	}
-	if diff := cmp.Diff(wantEntryTag, entryTags, cmpopts.IgnoreFields(tag.EntryTag{}, "ID")); diff != "" {
-		t.Errorf("EntryTags mismatch (-want +got):\n%s", diff)
-	}
-}
-
-func TestSaveTagsOldTags(t *testing.T) {
-	dbTestPath, db := initDB()
-	defer os.Remove(dbTestPath)
-
-	initEntry := entry.Entry{
-		Title: "Something something #tagone",
-	}
-	err := initializeEntry(db, initEntry)
-	if err != nil {
-		t.Fatalf("error saving initial entry: %v", err)
-	}
-
-	initTags := []tag.Tag{
-		{
-			Name: "tagold",
-		},
-	}
-	err = initializeTags(db, initTags, err)
-	if err != nil {
-		t.Fatalf("error saving initial tags: %v", err)
-	}
-
-	initEntryTags := []tag.EntryTag{
-		{
-			EntryID: initEntry.ID,
-			TagID:   initTags[0].ID,
-		},
-	}
-	err = initializeEntryTags(db, initEntryTags, err)
-	if err != nil {
-		t.Fatalf("error saving initial entryTags: %v", err)
-	}
-
-	err = SaveTags(db, initEntry)
-	if err != nil {
-		t.Fatalf("Error saving tags: %v", err)
-	}
-
-	tags := []tag.Tag{}
-	db.Model(tag.Tag{}).Find(&tags)
-	wantTags := []tag.Tag{
-		{
-			Name: "tagold",
+			name:           "new_entry",
+			initEntryTitle: "Something something #tagone",
+			initTags:       []string{},
+			initTagEntries: []string{},
+			wantTags:       []string{"tagone"},
+			wantTagEntries: []string{"tagone"},
 		},
 		{
-			Name: "tagone",
+			name:           "new_entry_two_tags",
+			initEntryTitle: "Something something #tagone #tagtwo",
+			initTags:       []string{},
+			initTagEntries: []string{},
+			wantTags:       []string{"tagone", "tagtwo"},
+			wantTagEntries: []string{"tagone", "tagtwo"},
 		},
-	}
-	if diff := cmp.Diff(wantTags, tags, cmpopts.IgnoreFields(tag.Tag{}, "ID")); diff != "" {
-		t.Errorf("Tags mismatch (-want +got):\n%s", diff)
+		{
+			name:           "existing_entry",
+			initEntryTitle: "Something something #tagone",
+			initTags:       []string{"tagone"},
+			initTagEntries: []string{"tagone"},
+			wantTags:       []string{"tagone"},
+			wantTagEntries: []string{"tagone"},
+		},
+		{
+			name:           "old_tags_are_removed",
+			initEntryTitle: "Something something #tagone #tagtwo",
+			initTags:       []string{"tagold", "tagtwo"},
+			initTagEntries: []string{"tagold", "tagtwo"},
+			wantTags:       []string{"tagold", "tagtwo", "tagone"},
+			wantTagEntries: []string{"tagone", "tagtwo"},
+		},
 	}
 
-	entryTags := []tag.EntryTag{}
-	db.Model(tag.EntryTag{}).Find(&entryTags)
-	wantEntryTag := []tag.EntryTag{
-		{
-			EntryID: initEntry.ID,
-			TagID:   getTagByName(tags, "tagone").ID,
-		},
-	}
-	if diff := cmp.Diff(wantEntryTag, entryTags, cmpopts.IgnoreFields(tag.EntryTag{}, "ID")); diff != "" {
-		t.Errorf("EntryTags mismatch (-want +got):\n%s", diff)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			dbTestPath, db := initDB()
+			defer os.Remove(dbTestPath)
+			e, err := initializeDBObjects(t, db, tc.initEntryTitle, tc.initTags, tc.initTagEntries)
+
+			err = SaveTags(db, *e)
+			if err != nil {
+				t.Fatalf("Error saving tags: %v", err)
+			}
+
+			tags := listTags(db)
+			err = verifyTags(tags, tc.wantTags)
+			if err != nil {
+				t.Fatalf("Error verifying tags: %v", err)
+			}
+			err = verifyTagEntries(db, tags, e, tc.wantTagEntries)
+			if err != nil {
+				t.Fatalf("Error verifying tags: %v", err)
+			}
+		})
 	}
 }
 
@@ -201,22 +89,80 @@ func initDB() (string, *gorm.DB) {
 	return dbTestPath, db
 }
 
-func initializeEntry(db *gorm.DB, initEntry entry.Entry) error {
-	return db.Save(&initEntry).Error
-}
+func initializeDBObjects(t *testing.T, db *gorm.DB, initEntryTitle string, initTags []string, initTagEntries []string) (*entry.Entry, error) {
+	// Initialize Entry
+	e := entry.Entry{
+		Title: initEntryTitle,
+	}
+	err := db.Save(&e).Error
+	if err != nil {
+		return nil, fmt.Errorf("Error saving entry: %v", err)
+	}
 
-func initializeTags(db *gorm.DB, initTags []tag.Tag, err error) error {
-	for _, initTag := range initTags {
+	// Initialize Tags
+	var initT []tag.Tag
+	for _, t := range initTags {
+		initT = append(initT, tag.Tag{Name: t})
+	}
+	for _, initTag := range initT {
 		err = db.Save(&initTag).Error
 	}
-	return err
+	if err != nil {
+		t.Fatalf("error saving initial tags: %v", err)
+	}
+
+	// Initialize EntryTags
+	var initET []tag.EntryTag
+	for _, teStr := range initTagEntries {
+		newET := tag.EntryTag{
+			EntryID: e.ID,
+			TagID:   getTagByName(initT, teStr).ID,
+		}
+		initET = append(initET, newET)
+	}
+	for _, et := range initET {
+		err = db.Save(&et).Error
+	}
+	if err != nil {
+		t.Fatalf("error saving initial entryTags: %v", err)
+	}
+	return &e, err
 }
 
-func initializeEntryTags(db *gorm.DB, initEntryTags []tag.EntryTag, err error) error {
-	for _, initET := range initEntryTags {
-		err = db.Save(&initET).Error
+func listTags(db *gorm.DB) []tag.Tag {
+	var tags []tag.Tag
+	db.Model(tag.Tag{}).Find(&tags)
+	return tags
+}
+
+func verifyTags(tags []tag.Tag, wantTagsStrs []string) error {
+	wantTags := []tag.Tag{}
+	for _, tStr := range wantTagsStrs {
+		wt := tag.Tag{Name: tStr}
+		wantTags = append(wantTags, wt)
 	}
-	return err
+	if diff := cmp.Diff(wantTags, tags, cmpopts.IgnoreFields(tag.Tag{}, "ID")); diff != "" {
+		return fmt.Errorf("tags mismatch (-want +got):\n%s", diff)
+	}
+	return nil
+}
+
+func verifyTagEntries(db *gorm.DB, tags []tag.Tag, e *entry.Entry, wantTagEntriesStrs []string) error {
+	entryTags := []tag.EntryTag{}
+	db.Model(tag.EntryTag{}).Find(&entryTags)
+	wantEntryTag := []tag.EntryTag{}
+	for _, teStr := range wantTagEntriesStrs {
+		newET := tag.EntryTag{
+			EntryID: e.ID,
+			TagID:   getTagByName(tags, teStr).ID,
+		}
+		wantEntryTag = append(wantEntryTag, newET)
+	}
+	if diff := cmp.Diff(wantEntryTag, entryTags, cmpopts.IgnoreFields(tag.EntryTag{}, "ID")); diff != "" {
+		return fmt.Errorf("EntryTags mismatch (-want +got):\n%s", diff)
+	}
+
+	return nil
 }
 
 func getTagByName(tags []tag.Tag, s string) tag.Tag {
