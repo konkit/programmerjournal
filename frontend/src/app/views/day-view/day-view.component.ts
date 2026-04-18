121a889d2a6074d93a3c74371e9c994874950dc4
Łukasz Tenerowicz <konkit@gmail.com>
2026-04-18 10:31:05 +0200

    Weekly and daily view side-by-side

--- frontend/src/app/views/day-view/day-view.component.ts
+++ frontend/src/app/views/day-view/day-view.component.ts
@@ -1,24 +1,25 @@
-import {Component, computed, effect, inject, OnInit, signal, ViewChild} from '@angular/core';
-import {EntryListService} from '../../service/entry-list.service';
-import {MatDrawer, MatDrawerContainer, MatDrawerContent} from '@angular/material/sidenav';
-import {CdkDragDrop, CdkDropList} from '@angular/cdk/drag-drop';
-import {Entry, TaskSummary} from '../../../frontend-client';
-import {EntryComponent} from '../../components/entry/entry.component';
-import {EntrySidebarComponent} from '../../components/entry-sidebar/entry-sidebar.component';
-import {NavToolbarComponent} from '../../components/nav-toolbar/nav-toolbar.component';
-import {NewEntryComponent} from '../../components/new-entry/new-entry.component';
-import {TitleWrapperComponent} from '../../components/title-wrapper/title-wrapper.component';
-import {ActivatedRoute} from '@angular/router';
-import {MatButton} from '@angular/material/button';
-import {RenderLinksPipe} from '../../components/entry/render-links.pipe';
-import {MatTooltip} from '@angular/material/tooltip';
-import {StatusIconComponent} from '../../components/status-icon/status-icon.component';
-import {MatIcon} from '@angular/material/icon';
-import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
-import {EntryStatus} from '../../../lib/entry';
-import {switchMap, tap} from 'rxjs';
-import {MatBadge} from '@angular/material/badge';
-import {WeekViewComponent} from '../week-view/week-view.component';
+import { Component, computed, effect, inject, OnInit, signal, ViewChild } from '@angular/core';
+import { EntryListService } from '../../service/entry-list.service';
+import { MatDrawer, MatDrawerContainer, MatDrawerContent } from '@angular/material/sidenav';
+import { CdkDragDrop, CdkDropList } from '@angular/cdk/drag-drop';
+import { Entry, TaskSummary } from '../../../frontend-client';
+import { EntryComponent } from '../../components/entry/entry.component';
+import { EntrySidebarComponent } from '../../components/entry-sidebar/entry-sidebar.component';
+import { NavToolbarComponent } from '../../components/nav-toolbar/nav-toolbar.component';
+import { NewEntryComponent } from '../../components/new-entry/new-entry.component';
+import { TitleWrapperComponent } from '../../components/title-wrapper/title-wrapper.component';
+import { ActivatedRoute } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { RenderLinksPipe } from '../../components/entry/render-links.pipe';
+import { MatTooltip } from '@angular/material/tooltip';
+import { StatusIconComponent } from '../../components/status-icon/status-icon.component';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { EntryStatus } from '../../../lib/entry';
+import { switchMap, tap } from 'rxjs';
+import { MatBadge } from '@angular/material/badge';
+import { WeekViewComponent } from '../week-view/week-view.component';
+import { getWeekString } from '../../../lib/wall_date';
 
 @Component({
   selector: 'app-day-view',
@@ -51,6 +52,7 @@ export class DayViewComponent implements OnInit {
 
   entryPriority1 = computed(() => this.entryListService.entryList().filter(e => e.rank < 0))
   nonPriority = computed(() => this.entryListService.entryList().filter(x => x.rank >= 0))
+  weeklyDateString = computed(() => getWeekString(new Date(this.entryListService.todayDate())))
 
   @ViewChild('drawer') sideDrawer!: MatDrawer;
   @ViewChild('weeklyDrawer') weeklyDrawer!: MatDrawer;
@@ -99,7 +101,7 @@ export class DayViewComponent implements OnInit {
   reloadTaskSummary(taskId: number) {
     // Update task summary
     this.entryListService.getTaskSummary(taskId)
-      .subscribe((ts) => {this.editedTaskSummary.set(ts)})
+      .subscribe((ts) => { this.editedTaskSummary.set(ts) })
 
     // Update task list, e.g. to update the status icons
     this.entryListService.refreshTasks().subscribe()
@@ -107,7 +109,7 @@ export class DayViewComponent implements OnInit {
 
   deleteTaskFromSidebar(taskId: number) {
     return this.entryListService.deleteEntry(taskId)
-      .subscribe(() => {this.sideDrawer.close()})
+      .subscribe(() => { this.sideDrawer.close() })
   }
 
   openWeeklySidebar() {
